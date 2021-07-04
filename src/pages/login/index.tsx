06ee662cbfa22f2eb51965a46e74632107304e00
doubj <935208113@qq.com>
2021-07-04 16:39:49 +0800

    登录页标题设置

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,6 +4,7 @@ import styles from './index.less';
 import { LoginParamsType, login } from '@/services/login';
 import useAsync from '@/utils/use-async';
 import { history } from 'umi';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 const Login: React.FC = () => {
   const [form] = Form.useForm();
@@ -21,38 +22,43 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <Card
-        className={styles.loginWrapper}
-        bodyStyle={{ padding: 0, height: '100%', display: 'flex' }}
-      >
-        <div className={styles.leftImage} />
-        <div className={styles.rightForm}>
-          <h1 className={'text-colorful'}>Money Tracing</h1>
-          <Form form={form} name="loginForm" initialValues={{}}>
-            <Form.Item
-              name="userName"
-              rules={[{ required: true, message: '用户名不能为空!' }]}
-            >
-              <Input size="large" placeholder="用户名" />
-            </Form.Item>
-            <Form.Item
-              name="password"
-              rules={[{ required: true, message: '请输入密码!' }]}
-            >
-              <Input.Password
-                size="large"
-                placeholder="密码"
-                onPressEnter={handleLogin}
-              />
-            </Form.Item>
-          </Form>
-          <Button loading={isLoading} onClick={handleLogin}>
-            登录
-          </Button>
-        </div>
-      </Card>
-    </div>
+    <HelmetProvider>
+      <Helmet>
+        <title>{`Money Tracing`}</title>
+      </Helmet>
+      <div className={styles.container}>
+        <Card
+          className={styles.loginWrapper}
+          bodyStyle={{ padding: 0, height: '100%', display: 'flex' }}
+        >
+          <div className={styles.leftImage} />
+          <div className={styles.rightForm}>
+            <h1 className={'text-colorful'}>Money Tracing</h1>
+            <Form form={form} name="loginForm" initialValues={{}}>
+              <Form.Item
+                name="userName"
+                rules={[{ required: true, message: '用户名不能为空!' }]}
+              >
+                <Input size="large" placeholder="用户名" />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                rules={[{ required: true, message: '请输入密码!' }]}
+              >
+                <Input.Password
+                  size="large"
+                  placeholder="密码"
+                  onPressEnter={handleLogin}
+                />
+              </Form.Item>
+            </Form>
+            <Button loading={isLoading} onClick={handleLogin}>
+              登录
+            </Button>
+          </div>
+        </Card>
+      </div>
+    </HelmetProvider>
   );
 };
 
